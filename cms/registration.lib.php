2d49911ce2d679037664ea4ffc2c276deb9baeba
Lokesh-784 <loke07082004@gmail.com>
2026-05-27 12:20:14 +0530

    registration form fix

--- cms/registration.lib.php
+++ cms/registration.lib.php
@@ -256,8 +256,8 @@ FORM;
 		} else {
 			$passwd = md5($_POST['user_password']);
 			$query = "INSERT INTO `" . MYSQL_DATABASE_PREFIX . "users` " .
-					"(`user_name`, `user_email`, `user_fullname`, `user_password`, `user_activated`) " .
-					"VALUES ('".escape(htmlentities($_POST['user_name']))."', '".escape(htmlentities($_POST['user_email']))."', '".escape(htmlentities($_POST['user_fullname']))."', '$passwd', ".ACTIVATE_USER_ON_REG.")";
+					"(`user_name`, `user_email`, `user_fullname`, `user_password`, `user_activated`, `user_lastlogin`) " .
+					"VALUES ('".escape(htmlentities($_POST['user_name']))."', '".escape(htmlentities($_POST['user_email']))."', '".escape(htmlentities($_POST['user_fullname']))."', '$passwd', ".ACTIVATE_USER_ON_REG.", CURRENT_TIMESTAMP)";
 			$result = mysqli_query($GLOBALS["___mysqli_ston"], $query);
 			$query1 = "SELECT `user_id` FROM `". MYSQL_DATABASE_PREFIX . "users` WHERE `user_email` ='".escape($_POST['user_email'])."' LIMIT 1";
 			$result1 = mysqli_query($GLOBALS["___mysqli_ston"], $query1);
