a4d2aa7155c77b211c074be0989db54b45e1870a
Lokesh-784 <loke07082004@gmail.com>
2026-06-03 12:31:24 +0530

    edited ckeditor config - to remove fonts option

--- cms/modules/article/ckeditor4.6/config.js
+++ cms/modules/article/ckeditor4.6/config.js
@@ -7,4 +7,31 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+	//turn on while editing stuff
+	config.allowedContent = true;
+
+	//comment out when edting source code
+	// config.removePlugins = 'sourcearea';
+	config.removeButtons = 'Font';
+
+
+	// Your existing settings (like config.extraAllowedContent) should remain here.
+
+	// 1. Prevent automatic filling of empty blocks (like <p> or <div>)
+	config.fillEmptyBlocks = false;
+
+	// 2. Prevent conversion of double spaces into &nbsp; + space
+	config.basicEntities = false;
+
+	// 3. Disable the automatic insertion of &nbsp; when the Tab key is pressed
+	config.tabSpaces = 0;
+
+	// 4. If you are pasting content, this removes hidden garbage from Word
+	config.pasteFromWordRemoveFontStyles = true;
+	config.pasteFromWordPromptCleanup = true;
+
+	// Use this setting with caution. It gives you more control but can lead to
+	// "loose text" if you're not careful with your HTML structure.
+	config.autoParagraph = false;
 };
